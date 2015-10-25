5f7ba91a1da33a0d977c2f925f97a7d156e1657d
Yi Chen <yi.chen.it@gmail.com>
2015-10-25 00:44:32 -0700

    added a varible for folder name

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,21 @@
 library(dplyr)
 
+## folder of the data set
+dataFolder = 'UCI HAR Dataset'
+
 ## load in the feature data, and extracts only the measurements on the mean and standard deviation
-featureDf <- read.csv(file.path('UCI HAR Dataset', 'features.txt'), sep = '', header = FALSE, col.names = c('index', 'feature'))
+featureDf <- read.csv(file.path(dataFolder, 'features.txt'), sep = '', header = FALSE, col.names = c('index', 'feature'))
 featureLogical <- grepl('mean\\(\\)|std\\(\\)', featureDf$feature)
 featureDf <- featureDf[featureLogical,]
 
 ## load in activity data to get the activity name
-activityDf <- read.csv(file.path('UCI HAR Dataset', 'activity_labels.txt'), sep = '', header = FALSE, col.names = c('index', 'activity'))
+activityDf <- read.csv(file.path(dataFolder, 'activity_labels.txt'), sep = '', header = FALSE, col.names = c('index', 'activity'))
 
 ## Attach subject ID; filter out irrelevant varibles from the data set for this analysis
 ## Uses descriptive activity names from the activity data to name the activities in the data set
 ## Labels the varibles in data set with feature names
 buildDataset <- function(partition, featureDf, featureLogical, activityDf) {
-  folder <- file.path("UCI HAR Dataset", partition)
+  folder <- file.path(dataFolder, partition)
   subject <- read.csv(file.path(folder, sprintf('subject_%s.txt', partition)), sep = '', header = FALSE, col.names = c('index'))
   observation <- read.csv(file.path(folder, sprintf('X_%s.txt', partition)), sep = '', header = FALSE, colClasses = sapply(featureLogical, function(x) if (x) 'numeric' else 'NULL'))
   activity <- read.csv(file.path(folder, sprintf('y_%s.txt', partition)), sep = '', header = FALSE, col.names = c('index'))
